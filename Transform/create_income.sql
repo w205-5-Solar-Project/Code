0b9676d552b60c5d08bfb40ff42c424e0b7b9f99
w205-solar-group <vslchu@ischool.berkeley.edu>
2016-11-10 20:31:52 -0800

    Add files via upload

--- Transform/create_income.sql
+++ Transform/create_income.sql
@@ -2,12 +2,15 @@
 -- W205 Section 5 Course Project
 -- Elizabeth Shulok
 -- File name: create_income.sql
--- Description: 
+-- Description:
 -- Date       : 11/6/2016
 ----------------------------------------------------------------------------
 DROP TABLE income;
 
 CREATE TABLE income AS
-SELECT DISTINCT UPPER(county_name) AS name,
-cast(2014_income AS BIGINT) AS 2014_income
+SELECT DISTINCT 
+IF(instr(county_name, ' County, California') > 0, 
+  UPPER(substring(county_name, 1, instr(county_name, ' County, California') - 1)),  
+  'UNKNOWN') AS county_name, 
+cast(2014_income AS DOUBLE) AS 2014_income
 FROM income_raw;
