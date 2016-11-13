d826778562ad2a254fd5054604cda1068bb2aa3c
w205-solar-group <vslchu@ischool.berkeley.edu>
2016-11-13 11:56:08 -0800

    Update create_usage.sql

--- Transform/create_usage.sql
+++ Transform/create_usage.sql
@@ -2,15 +2,15 @@
 -- W205 Section 5 Course Project
 -- Elizabeth Shulok
 -- File name: create_usage.sql
--- Description: 
+-- Description:
 -- Date       : 11/6/2016
 ----------------------------------------------------------------------------
 DROP TABLE usage;
 
 CREATE TABLE usage AS
-SELECT DISTINCT UPPER(name) AS name,
-UPPER(sector) AS sector, 
-cast(yr_2015 AS DOUBLE) AS yr_2015, 
+SELECT DISTINCT UPPER(name) AS county_name,
+UPPER(sector) AS sector,
+cast(yr_2015 AS DOUBLE) AS yr_2015,
 cast(yr_2014 AS DOUBLE) AS yr_2014,
 cast(yr_2013 AS DOUBLE) AS yr_2013
 FROM usage_raw;
