166af71189438c19a713d756f0fbafd82cc8d4c2
w205-solar-group <vslchu@ischool.berkeley.edu>
2016-11-13 19:11:33 -0800

    Add files via upload

---
+++ Transform/create_interconnection_by_county.sql
@@ -0,0 +1,18 @@
+----------------------------------------------------------------------------
+-- W205 Section 5 Course Project
+-- Vincent Chu
+-- File name  : create_interconnection_by_county.sql
+-- Description: 
+-- Date       : 11/13/2016
+----------------------------------------------------------------------------
+DROP TABLE interconnection_by_county;
+
+CREATE TABLE interconnection_by_county AS
+SELECT DISTINCT county,
+SUM(size_dc) AS total_size_dc,
+SUM(size_ac) AS total_size_ac, 
+SUM(ev_count) AS total_ev_count,
+AVG(total_system_cost) AS avg_system_cost
+FROM interconnection
+GROUP BY county
+ORDER by county;
