8c4320160700264cb80de051913adc17b8f9c630
eshulok <shulok@berkeley.edu>
2016-12-09 00:42:25 -0800

    Tableau file

--- Transform/create_interconnection_by_county.sql
+++ Transform/create_interconnection_by_county.sql
@@ -4,11 +4,12 @@
 -- File name  : create_interconnection_by_county.sql
 -- Description: 
 -- Date       : 11/13/2016
+-- Modified   : 12/07/2016-ES: removed DISTINCT after select
 ----------------------------------------------------------------------------
 DROP TABLE interconnection_by_county;
 
 CREATE TABLE interconnection_by_county AS
-SELECT DISTINCT county,
+SELECT county,
 SUM(size_dc) AS total_size_dc,
 SUM(size_ac) AS total_size_ac, 
 SUM(ev_count) AS total_ev_count,
